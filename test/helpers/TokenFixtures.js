96b7457a346ed6318897b6613be9427d7fec3c0a
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-05 09:30:17 +0100

    feat: include deployer account in deployTokenFixture return value

--- test/helpers/TokenFixtures.js
+++ test/helpers/TokenFixtures.js
@@ -2,7 +2,10 @@ async function deployTokenFixture() {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy("Dapp University", "DAPP", "1000000");
 
-    return { token }
+    const accounts = await ethers.getSigners();
+    const deployer = accounts[0];
+
+    return { token, deployer }
 }
 
 module.exports = {
