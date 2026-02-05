bbf1bcc9ce4ba181f8f66f638407d440806c3f9e
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-05 13:06:09 +0100

    feat: implement success and failure scenarios for token transfer functionality

--- test/Token.js
+++ test/Token.js
@@ -45,18 +45,57 @@ describe("Token", () => {
 
     describe("Sending Tokens", () => {
         const AMOUNT = tokens(100);
-        it("transfer token balances", async() => {
-            const { token, deployer, receiver } = await loadFixture(deployTokenFixture);
+        
+        describe("Success", () => {
+            it("transfer token balances", async() => {
+                const { token, deployer, receiver } = await loadFixture(deployTokenFixture);
 
+                // Do transfer
+                const transaction = await token.connect(deployer).transfer(receiver.address, AMOUNT);
+                await transaction.wait();
+                
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
+                expect(await token.balanceOf(receiver.address)).to.equal(AMOUNT);
 
-            // Do transfer
-            const transaction = await token.connect(deployer).transfer(receiver.address, AMOUNT);
-            await transaction.wait();
-            
-            expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
-            expect(await token.balanceOf(receiver.address)).to.equal(AMOUNT);
+            });
 
-        });
+            it("emits a Transfer event", async() => {
+                const { token, deployer, receiver } = await loadFixture(deployTokenFixture);
+
+                // Do transfer
+                const transaction = await token.connect(deployer).transfer(receiver.address, AMOUNT);
+                await transaction.wait();            
+                
+                await expect(transaction).to.emit(token, "Transfer")
+                    .withArgs(deployer.address, receiver.address, AMOUNT);
+
+            });
+        }); // Describe Success
+        
+        describe("Failure", () => {
+            it("rejects insufficient balances", async() => {
+                const { token, deployer, receiver } = await loadFixture(deployTokenFixture);
+
+                const INVALID_AMOUNT = tokens(100000000);
+                const ERROR = "Token: Insufficient Funds";
+
+                await expect(token.connect(deployer)
+                    .transfer(receiver.address, INVALID_AMOUNT))
+                    .to.be.revertedWith(ERROR);
+
+            });
+
+            it("rejects invalid recipient", async() => {
+                const { token, deployer, receiver } = await loadFixture(deployTokenFixture);
+
+                const INVALID_ADDRESS = "0x0000000000000000000000000000000000000000";
+                const ERROR = "Token: Recipient is address 0";
+
+                await expect(token.connect(deployer)
+                    .transfer(INVALID_ADDRESS, AMOUNT))
+                    .to.be.revertedWith(ERROR);
+            });
+        }); // Describe Failure
     }); // Describe Sending Tokens
     
 }); // Describe Token
