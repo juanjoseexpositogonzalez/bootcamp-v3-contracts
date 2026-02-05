9b05d7d8b63f1a9ef196c12876b4625136b6704a
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-05 08:37:03 +0100

    feat: add deployTokenFixture helper function for token deployment

---
+++ test/helpers/TokenFixtures.js
@@ -0,0 +1,10 @@
+async function deployTokenFixture() {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy("Dapp University", "DAPP", "1000000");
+
+    return { token }
+}
+
+module.exports = {
+    deployTokenFixture
+}
