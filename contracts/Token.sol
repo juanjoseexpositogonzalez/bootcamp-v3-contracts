e7fad5ca083210c8a304101efccb28a6ad647e6e
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-05 13:06:03 +0100

    feat: add Transfer event emission and require checks in transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,8 @@ contract Token {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -24,11 +26,19 @@ contract Token {
         address _to,
         uint256 _value
     ) public returns (bool success) {
+        // 0. Require caller's account balance to be greater than or equal to _value
+        require(balanceOf[msg.sender] >= _value, "Token: Insufficient Funds");
+        require(_to != address(0), "Token: Recipient is address 0");
+
         // 1. Deduct tokens from sender
         balanceOf[msg.sender] -= _value;
-        
+
         // 2. Credit tokens to recipient
         balanceOf[_to] += _value;
+
+        // 3. Emit Transfer Event
+        emit Transfer(msg.sender, _to, _value);
+
         return true;
     }
 }
