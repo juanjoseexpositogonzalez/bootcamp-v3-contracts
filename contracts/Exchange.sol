d2e6db1fccbbbac231edec451b5601717df0ce14
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-09 06:24:35 +0100

    feat: implement withdraw functionality and event emission for token withdrawals in Exchange contract

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -19,6 +19,13 @@ contract Exchange {
         uint256 balance
     );
 
+    event TokensWithdrawn(
+        address indexed token,
+        address indexed user,
+        uint256 amount,
+        uint256 balance
+    );
+
     constructor( 
         address _feeAccount,       
         uint256 _feePercent
@@ -48,6 +55,33 @@ contract Exchange {
         // Transfer tokens to exchange
         require(
             Token(_token).transferFrom(msg.sender, address(this), _amount),
+            "Exchange: Token transfer failed"
+        );
+    }
+
+    function withdrawToken(
+        address _token,
+        uint256 _amount
+    ) public {
+        require(
+            totalBalanceOf(_token, msg.sender) >= _amount,
+            "Exchange: Insufficient balance"
+        );
+
+        // Update user balance
+        userTotalTokenBalance[_token][msg.sender] -= _amount;
+
+        // Emite an Event
+        emit TokensWithdrawn(
+            _token,
+            msg.sender,
+            _amount,
+            userTotalTokenBalance[_token][msg.sender]
+        );
+
+        // Transfer tokens back to user
+        require(
+            Token(_token).transfer(msg.sender, _amount),
             "Exchange: Token transfer failed"
         );
     }
