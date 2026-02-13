05575d09b413d6f4ac0c3b5c101cffd8306cce8b
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-13 06:52:12 +0100

    feat: Enhance flashLoan function with balance checks and error handling

--- contracts/FlashLoanProvider.sol
+++ contracts/FlashLoanProvider.sol
@@ -3,6 +3,14 @@ pragma solidity 0.8.28;
 
 import { Token } from "./Token.sol";
 
+interface IFlashLoanReceiver {
+    function receiveFlashLoan(
+        address token,
+        uint256 amount,
+        bytes memory data
+    ) external;
+}
+
 // We don't want to deploy this contract. We only want
 // our Exchange contract to inherit this contract,
 // thus we make it abstract
@@ -18,12 +26,36 @@ abstract contract FlashLoanProvider {
         uint256 _amount,
         bytes memory _data
     ) public {
+        // Get our current token balance
+        uint256 tokenBalanceBefore = Token(_token).balanceOf(address(this));
+
+        // Require this contract to have sufficient funds to send
+        require(
+            tokenBalanceBefore > 0,
+            "FlashLoanProvider: Insufficient funds to loan"
+        );
+
         // Send the money
-        Token(_token).transfer(msg.sender, _amount);
+        require(
+            Token(_token).transfer(msg.sender, _amount),
+            "FlashLoanProvider: Transfer failed"
+        );
 
-        // Ask for money back
+        // Call receiveFlashLoan() on msg.sender -> callback function
+        IFlashLoanReceiver(msg.sender).receiveFlashLoan(
+            _token,
+            _amount,
+            _data
+        );
 
-        // Ensure that money was paid back
+        // Get our token balance after
+        uint256 tokenBalanceAfter = Token(_token).balanceOf(address(this));
+
+        // Require this contract to have received back the funds
+        require(
+            tokenBalanceAfter >= tokenBalanceBefore,
+            "FlashLoanProvider: Funds not received"
+        );
 
         // Emit an Event
         emit FlashLoan(_token, _amount, block.timestamp);
