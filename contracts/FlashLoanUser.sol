cba51fbc00a20c7a101a9eae54f0b812bc3278a1
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-02-13 06:52:19 +0100

    feat: Implement FlashLoanUser contract with receiveFlashLoan functionality and event emission

--- contracts/FlashLoanUser.sol
+++ contracts/FlashLoanUser.sol
@@ -3,21 +3,40 @@ pragma solidity 0.8.28;
 
 import { Token } from "./Token.sol";
 import { Exchange } from "./Exchange.sol";
+import { IFlashLoanReceiver } from "./FlashLoanProvider.sol";
 
-contract FlashLoanUser {
+contract FlashLoanUser is IFlashLoanReceiver {
     address exchange;
+
+    event FlashLoanReceived(address token, uint256 amount);
 
     constructor(address _exchange) {
         exchange = _exchange;
     }
-
 
     function getFlashLoan(
         address _token,
         uint256 _amount
     ) external {        
         // Call exchange for flash loan
-        Exchange(exchange).flashLoan(_token, _amount, "");
+        Exchange(exchange).flashLoan(_token, _amount, "");        
+    }
+
+    function receiveFlashLoan(
+        address _token, 
+        uint256 _amount, 
+        bytes memory /* _data */
+    ) external {
+        require(msg.sender == exchange, "FlashLoanUser: Not exchange contract");
+        
+        // Do something with our loan
+        emit FlashLoanReceived(_token, Token(_token).balanceOf(address(this)));
+
+        // Pay back loan
+        require(
+            Token(_token).transfer(exchange, _amount),
+            "FlashLoanUser: Token transfer failed"
+        );
     }
 
 }
